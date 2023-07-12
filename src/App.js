8f8d01953430204d12dd4aa61ba868a6e5a0012a
victorsworld <victordirect48@gmail.com>
2023-07-12 12:20:40 -0400

    initial Setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+const url = process.env.REACT_APP_URL_ENDPOINT;
+
+
+
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ToDoCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ToDoCard = () => {
+  return (
+    <div>ToDoCard</div>
+  )
+}
+
+export default ToDoCard
